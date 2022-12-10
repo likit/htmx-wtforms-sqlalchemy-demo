d15669db508ed12da744cb7f4ed25b2dc68d612d
Likit Preeyanon <likit.pre@mahidol.edu>
2022-12-11 02:51:44 +0700

    added a dynamic login/signup form with htmx

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,4 +6,3 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,26 @@
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for, make_response
 from app.main import main_blueprint as main
 from app.main.forms import LoginForm
 from app import db
+import time
 from app.main.models import User
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/', methods=['GET'])
 def index():
     form = LoginForm()
+    return render_template('main/index.html', form=form)
+
+
+@main.route('/api/login', methods=['POST'])
+def process_login_form():
+    form = LoginForm()
+    time.sleep(3)
     if form.validate_on_submit():
         user = User.query.get(form.email.data)
         if user:
             if user.password == form.password.data:
-                flash('Login successfully', 'success')
+                flash('Login Successfully.', 'success')
             else:
                 flash('The email already exists.', 'danger')
         else:
@@ -21,10 +29,13 @@ def index():
             user.password = form.password.data
             db.session.add(user)
             db.session.commit()
-            flash('Sign up successfully.', 'success')
+            flash('Sign Up Successfully.', 'success')
     else:
         for field, err in form.errors.items():
             flash(f'{field}:{err}', 'danger')
 
-    return render_template('main/index.html', form=form)
+    resp = make_response()
+    resp.headers['HX-Redirect'] = url_for('main.index')
+    return resp
+
 
